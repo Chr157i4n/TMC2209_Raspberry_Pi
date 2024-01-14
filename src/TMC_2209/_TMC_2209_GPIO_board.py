e1c6561433cb905e35d8ddd8ca78bfca9dae8d83
protsey <protseyuriy@gmail.com>
2024-01-15 01:49:46 +0200

    refactor

--- src/TMC_2209/_TMC_2209_GPIO_board.py
+++ src/TMC_2209/_TMC_2209_GPIO_board.py
@@ -3,7 +3,9 @@ Many boards have RaspberryPI-compatible PinOut,
 but require to import special GPIO module instead RPI.GPIO
 
 This module determines the type of board
-and installs the corresponding GPIO module
+and import the corresponding GPIO module/
+
+Can be extended to support BeagleBone or other boards
 """
 
 from ._TMC_2209_logger import TMC_logger, Loglevel
@@ -17,24 +19,20 @@ with open('/proc/device-tree/model', encoding="utf-8") as f:
             BOARD = "RASPBERRY_PI"
         except ModuleNotFoundError as err:
             dependencies_logger.log(
-f'''
-ModuleNotFoundError: {err}
-Board is Raspberry PI but module RPi.GPIO isn`t installed.
-Follow the installation instructions in the link below to resolve the issue:
-https://sourceforge.net/p/raspberry-gpio-python/wiki/install/
-
-Exiting...''',
+                (f"ImportError: {err}\n"
+                "Board is Raspberry PI but module RPi.GPIO isn`t installed.\n"
+                "Follow the installation instructions in the link below to resolve the issue:\n"
+                "https://sourceforge.net/p/raspberry-gpio-python/wiki/install/\n"
+                "Exiting..."),
             Loglevel.ERROR)
             raise
         except ImportError as err:
             dependencies_logger.log(
-f'''
-ImportError: {err}
-Board is Raspberry PI but module RPi.GPIO isn`t installed.
-Follow the installation instructions in the link below to resolve the issue:
-https://sourceforge.net/p/raspberry-gpio-python/wiki/install/
-
-Exiting...''',
+                (f"ImportError: {err}\n"
+                "Board is Raspberry PI but module RPi.GPIO isn`t installed.\n"
+                "Follow the installation instructions in the link below to resolve the issue:\n"
+                "https://sourceforge.net/p/raspberry-gpio-python/wiki/install/\n"
+                "Exiting..."),
             Loglevel.ERROR)
             raise
     elif "nvidia jetson" in model:
@@ -43,24 +41,20 @@ Exiting...''',
             BOARD = "NVIDIA_JETSON"
         except ModuleNotFoundError as err:
             dependencies_logger.log(
-f'''
-ModuleNotFoundError: {err}
-Board is Nvidia Jetson but module jetson-gpio isn`t installed.
-Follow the installation instructions in the link below to resolve the issue:
-https://github.com/NVIDIA/jetson-gpio
-
-Exiting...''',
+                (f"ModuleNotFoundError: {err}\n"
+                "Board is Nvidia Jetson but module jetson-gpio isn`t installed.\n"
+                "Follow the installation instructions in the link below to resolve the issue:\n"
+                "https://github.com/NVIDIA/jetson-gpio\n"
+                "Exiting..."),
             Loglevel.ERROR)
             raise
         except ImportError as err:
             dependencies_logger.log(
-f'''
-ImportError: {err}
-Board is Nvidia Jetson but module jetson-gpio isn`t installed.
-Follow the installation instructions in the link below to resolve the issue:
-https://github.com/NVIDIA/jetson-gpio
-
-Exiting...''',
+                (f"ModuleNotFoundError: {err}\n"
+                "Board is Nvidia Jetson but module jetson-gpio isn`t installed.\n"
+                "Follow the installation instructions in the link below to resolve the issue:\n"
+                "https://github.com/NVIDIA/jetson-gpio\n"
+                "Exiting..."),
             Loglevel.ERROR)
             raise
     else:
