889c6f102c23943c11885d192cf30520fa8c3639
Christian Köhlke <christian@koehlke.de>
2024-01-15 17:34:36 +0000

    fix for testing the code on os where /proc/device-tree/model does not exist

--- src/TMC_2209/_TMC_2209_GPIO_board.py
+++ src/TMC_2209/_TMC_2209_GPIO_board.py
@@ -9,61 +9,66 @@ Can be extended to support BeagleBone or other boards
 """
 
 from ._TMC_2209_logger import TMC_logger, Loglevel
+from os.path import exists
 
 dependencies_logger = TMC_logger(Loglevel.DEBUG, "DEPENDENCIES")
-with open('/proc/device-tree/model', encoding="utf-8") as f:
-    model = f.readline().lower()
-    if "raspberry" in model:
-        try:
-            from RPi import GPIO
-            BOARD = "RASPBERRY_PI"
-        except ModuleNotFoundError as err:
+
+if not exists('/proc/device-tree/model'):
+    from Mock import GPIO
+else:
+    with open('/proc/device-tree/model', encoding="utf-8") as f:
+        model = f.readline().lower()
+        if "raspberry" in model:
+            try:
+                from RPi import GPIO
+                BOARD = "RASPBERRY_PI"
+            except ModuleNotFoundError as err:
+                dependencies_logger.log(
+                    (f"ModuleNotFoundError: {err}\n"
+                    "Board is Raspberry PI but module RPi.GPIO isn`t installed.\n"
+                    "Follow the installation instructions in the link below to resolve the issue:\n"
+                    "https://sourceforge.net/p/raspberry-gpio-python/wiki/install/\n"
+                    "Exiting..."),
+                Loglevel.ERROR)
+                raise
+            except ImportError as err:
+                dependencies_logger.log(
+                    (f"ImportError: {err}\n"
+                    "Board is Raspberry PI but module RPi.GPIO isn`t installed.\n"
+                    "Follow the installation instructions in the link below to resolve the issue:\n"
+                    "https://sourceforge.net/p/raspberry-gpio-python/wiki/install/\n"
+                    "Exiting..."),
+                Loglevel.ERROR)
+                raise
+        elif "nvidia jetson" in model:
+            try:
+                from Jetson import GPIO
+                BOARD = "NVIDIA_JETSON"
+            except ModuleNotFoundError as err:
+                dependencies_logger.log(
+                    (f"ModuleNotFoundError: {err}\n"
+                    "Board is Nvidia Jetson but module jetson-gpio isn`t installed.\n"
+                    "Follow the installation instructions in the link below to resolve the issue:\n"
+                    "https://github.com/NVIDIA/jetson-gpio\n"
+                    "Exiting..."),
+                Loglevel.ERROR)
+                raise
+            except ImportError as err:
+                dependencies_logger.log(
+                    (f"ImportError: {err}\n"
+                    "Board is Nvidia Jetson but module jetson-gpio isn`t installed.\n"
+                    "Follow the installation instructions in the link below to resolve the issue:\n"
+                    "https://github.com/NVIDIA/jetson-gpio\n"
+                    "Exiting..."),
+                Loglevel.ERROR)
+                raise
+        else:
+            # just in case
             dependencies_logger.log(
-                (f"ModuleNotFoundError: {err}\n"
-                "Board is Raspberry PI but module RPi.GPIO isn`t installed.\n"
-                "Follow the installation instructions in the link below to resolve the issue:\n"
-                "https://sourceforge.net/p/raspberry-gpio-python/wiki/install/\n"
-                "Exiting..."),
-            Loglevel.ERROR)
-            raise
-        except ImportError as err:
-            dependencies_logger.log(
-                (f"ImportError: {err}\n"
-                "Board is Raspberry PI but module RPi.GPIO isn`t installed.\n"
-                "Follow the installation instructions in the link below to resolve the issue:\n"
-                "https://sourceforge.net/p/raspberry-gpio-python/wiki/install/\n"
-                "Exiting..."),
-            Loglevel.ERROR)
-            raise
-    elif "nvidia jetson" in model:
-        try:
-            from Jetson import GPIO
-            BOARD = "NVIDIA_JETSON"
-        except ModuleNotFoundError as err:
-            dependencies_logger.log(
-                (f"ModuleNotFoundError: {err}\n"
-                "Board is Nvidia Jetson but module jetson-gpio isn`t installed.\n"
-                "Follow the installation instructions in the link below to resolve the issue:\n"
-                "https://github.com/NVIDIA/jetson-gpio\n"
-                "Exiting..."),
-            Loglevel.ERROR)
-            raise
-        except ImportError as err:
-            dependencies_logger.log(
-                (f"ImportError: {err}\n"
-                "Board is Nvidia Jetson but module jetson-gpio isn`t installed.\n"
-                "Follow the installation instructions in the link below to resolve the issue:\n"
-                "https://github.com/NVIDIA/jetson-gpio\n"
-                "Exiting..."),
-            Loglevel.ERROR)
-            raise
-    else:
-        # just in case
-        dependencies_logger.log(
-            "The board is not recognized. Trying import default RPi.GPIO module...",
-             Loglevel.INFO)
-        BOARD = "UNKNOWN"
-        try:
-            from RPi import GPIO
-        except ImportError:
-            from Mock import GPIO
+                "The board is not recognized. Trying import default RPi.GPIO module...",
+                Loglevel.INFO)
+            BOARD = "UNKNOWN"
+            try:
+                from RPi import GPIO
+            except ImportError:
+                from Mock import GPIO
