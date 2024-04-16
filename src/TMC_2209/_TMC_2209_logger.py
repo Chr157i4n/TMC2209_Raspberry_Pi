704e6c304f37abbfa7ddb09b885c69686cf41175
SiboVG <sibo.vangool@hotmail.com>
2024-04-16 22:01:25 +0200

    Delete leftover print debug statements (sorry!)

--- src/TMC_2209/_TMC_2209_logger.py
+++ src/TMC_2209/_TMC_2209_logger.py
@@ -43,7 +43,6 @@ class TMC_logger:
 
         # Add our custom log levels to the logger
         for level in [Loglevel.ALL, Loglevel.MOVEMENT, Loglevel.NONE]:
-            print(level)
             self._add_logging_level(level.name, level.value)
 
         self.logger = logging.getLogger(logprefix)
@@ -139,7 +138,6 @@ class TMC_logger:
 
         def logForLevel(self, message, *args, **kwargs):
             if self.isEnabledFor(level_num):
-                print("test")
                 self._log(level_num, message, args, **kwargs)
 
         def logToRoot(message, *args, **kwargs):
